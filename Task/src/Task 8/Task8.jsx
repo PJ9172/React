3930b090c3f2585ecbbade46c5757fa61a9b82a8
Prajwal <rohitjagtap5000@gmail.com>
2024-06-13 01:28:54 +0530

    Task 8 Completed !!!

--- Task/src/Task 8/Task8.jsx
+++ Task/src/Task 8/Task8.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import './Task8.css'
+
 
 export default function Task8() {
     const [newInput, setInput] = new useState("")
@@ -19,10 +21,10 @@ export default function Task8() {
         var result = "";
         for (var i = 0; i < currentInput.length; i++) {
             var charcode = currentInput.charCodeAt(i);
-            if(charcode>=97 && charcode<=122)
+            if (charcode >= 97 && charcode <= 122)
                 result += String.fromCharCode(charcode & 223);
             else
-                result+=currentInput[i];
+                result += currentInput[i];
         }
         return result;
     }
@@ -45,20 +47,73 @@ export default function Task8() {
                 out = currentInput.replace(/ /g, "-");
                 setInput(lower(out));
             }
+            if (wcase === "camelcase") {
+                currentInput = lower(currentInput);
+                var charcode = currentInput.charCodeAt(0);
+                if (charcode >= 65 && charcode <= 90)
+                    out += String.fromCharCode(charcode + 32);
+                else
+                    out += currentInput[0];
+                for (var i = 1; i < currentInput.length; i++) {
+                    if (currentInput[i] === " ") {
+                        i++;
+                        charcode = currentInput.charCodeAt(i);
+                        if (charcode >= 97 && charcode <= 122)
+                            out += String.fromCharCode(charcode & 223);
+                        else
+                            out += currentInput[i];
+                    }
+                    else
+                        out += currentInput[i];
+                }
+                setInput(out);
+            }
+            if (wcase === "pascalcase") {
+                currentInput = lower(currentInput);
+                var charcode = currentInput.charCodeAt(0);
+                if (charcode >= 97 && charcode <= 122)
+                    out += String.fromCharCode(charcode & 223);
+                else
+                    out += currentInput[0];
+                for (var i = 1; i < currentInput.length; i++) {
+                    if (currentInput[i] === " ") {
+                        i++;
+                        charcode = currentInput.charCodeAt(i);
+                        if (charcode >= 97 && charcode <= 122)
+                            out += String.fromCharCode(charcode & 223);
+                        else
+                            out += currentInput[i];
+                    }
+                    else
+                        out += currentInput[i];
+                }
+                setInput(out);
+            }
         }
+        else
+            alert("Please Enter Valid String ! ! !")
     }
 
     return (
         <>
-            <input type="text" placeholder="Enter Input String ! ! !" id="input" />
-            <button onClick={() => { changeCase("lowercase") }}>lowercase</button>
-            <button onClick={() => { changeCase("uppercase") }}>UPPERCASE</button>
-            <button onClick={() => { changeCase("camelcase") }}>camelCase</button>
-            <button onClick={() => { changeCase("pascalcase") }}>PascalCase</button>
-            <button onClick={() => { changeCase("snakecase") }}>snake_case</button>
-            <button onClick={() => { changeCase("kebabcase") }}>kebab-case</button>
-            <br /><br />
-            <h1>{newInput}</h1>
+            <center>
+                <div className="container">
+                    <div className="input">
+                        <input type="text" placeholder="Enter Input String ! ! !" id="input" />
+                    </div>
+                    <div className="casebutton">
+                        <button onClick={() => { changeCase("lowercase") }}>lowercase</button>
+                        <button onClick={() => { changeCase("uppercase") }}>UPPERCASE</button>
+                        <button onClick={() => { changeCase("camelcase") }}>camelCase</button>
+                        <button onClick={() => { changeCase("pascalcase") }}>PascalCase</button>
+                        <button onClick={() => { changeCase("snakecase") }}>snake_case</button>
+                        <button onClick={() => { changeCase("kebabcase") }}>kebab-case</button>
+                    </div>
+                    <div className="output">
+                        <h1>{newInput}</h1>
+                    </div>
+                </div>
+            </center>
         </>
     )
 }
